8fc2c3d8bbd6a8f54f74a4bd32d65aa64a9e83b8
jonibek0979 <omadbekxoshimov20060715@gmail.com>
2023-09-25 12:55:31 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+// var firstName = "Usnon";
+// var first_name = "Usnon";
+// var FirstName = "Usnon";
+ var str = "salom"
+ var num = 100
+ var bul = true // false
+ var un = undefined
+ var n = null
+ var sm = Symbol('a')
+ var obj = {}
+
+
+
+alert('Xush kelibsiz')
+alert("O'zinggiz haqinggizdagi malumotlarni kiriting")
+var age = prompt ('Yoshinggiz kiriting')
+var num = prompt ('Telefon raqaminggiz kiriting')
+var str = prompt ("Yashash manzilinggizni kiriting")
+console.log(age);
+console.log(num);
+console.log(str);
+// num = prompt ('num')
+// var num1 = prompt('num1')
+// var num2 = prompt('num2')
+// console.log( num1 * num2 )
+// console.log( num1 / num2 )
+// console.log( num1 * 1 + num2 * 1)
+// console.log( num1 - num2 )
+// console.log( tel )
+// console.log("salom" * 1 + 10);
+// console.log("10" * 1 + 10);
+// console.log("10" - 10);
+// console.log("10" * 10);
+// console.log("10" / 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
